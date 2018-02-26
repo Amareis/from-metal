9fd7ebec34cf0d92cbe3919ecc486f4ae5af892d
Ivan Plesskih <terma95@gmail.com>
2018-02-26 19:53:17 +0500

    WIP

--- src/app-root.vue
+++ src/app-root.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input v-model="rawInput" placeholder="New memory" />
+    <textarea v-model="rawInput" placeholder="New memory"></textarea>
     <button @click="newMemory">Set memory</button> <br>
 
     <input id="divOps" type="checkbox" v-model="divideOperations" />
@@ -34,12 +34,65 @@ const proc = {
   op1(arg) {
     this.nextOpAddr = arg
   },
+
   op2(arg) {
     this.set(arg, this.get(arg) + 1)
   },
+  op20(arg1, arg2) {
+    this.set(arg1, this.get(arg1) + arg2)
+  },
+  op21(arg1, arg2) {
+    this.set(arg1, this.get(arg1) + this.get(arg2))
+  },
+
   op3(arg) {
     this.set(arg, this.get(arg) - 1)
   },
+  op30(arg1, arg2) {
+    this.set(arg1, this.get(arg1) - arg2)
+  },
+  op31(arg1, arg2) {
+    this.set(arg1, this.get(arg1) - this.get(arg2))
+  },
+
+  op40(arg1, arg2) {
+    this.set(arg1, this.get(arg1) * arg2)
+  },
+  op41(arg1, arg2) {
+    this.set(arg1, this.get(arg1) * this.get(arg2))
+  },
+
+  op50(arg1, arg2) {
+    this.set(arg1, Math.floor(this.get(arg1) / arg2))
+  },
+  op51(arg1, arg2) {
+    this.set(arg1, Math.floor(this.get(arg1) / this.get(arg2)))
+  },
+
+  op60(arg1, arg2) {
+    this.set(arg1, this.get(arg1) % arg2)
+  },
+  op61(arg1, arg2) {
+    this.set(arg1, this.get(arg1) % this.get(arg2))
+  },
+
+  op7(arg1, arg2) {
+    this.set(arg1, this.get(arg2))
+  },
+  op70(arg1, arg2) {
+    this.diff = this.get(arg1) - this.get(arg2)
+  },
+
+  op8(arg) {
+    alert(this.get(arg))
+  },
+  op9(arg) {
+    let num
+    do {
+      num = parseInt(prompt())
+    } while(isNaN(num))
+    this.set(arg, num)
+  }
 }
 
 const initMemory = [2,6, 2,6, 1,6, 0, 2,0, 1,0]
@@ -48,6 +101,7 @@ module.exports = {
   data() {
     return {
       nextOpAddr: 0,
+      diff: 0,
       memory: initMemory,
       divideOperations: true,
       rawInput: initMemory.join(' ')
