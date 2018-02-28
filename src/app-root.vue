df709fe8a41abfb088cceb8930c4dc795d7434a0
Ivan Plesskih <terma95@gmail.com>
2018-02-28 20:09:17 +0500

    WIP

--- src/app-root.vue
+++ src/app-root.vue
@@ -22,131 +22,52 @@
     <button @click="step">One step</button>
     <br><br>
     <br><br>
-    <button @click="newMemory">Set rules</button> <br> <br>
+    <!--button @click="newMemory">Set rules</button> <br> <br-->
     <textarea v-model="rawRules" placeholder="Rules"></textarea>
-    <div v-for="rule of rules" class="rule">{{rule}}</div>
+    <!--div v-for="(rule, opcode) of rules" class="rule">{{opcode}} {{rule}}</div-->
   </div>
 </template>
 
 <script>
 const rawRules = `
 0   nop
+
 1   IP = $1
 10  if (D == 0) IP = $1
+100 if (D != 0) IP = $1
+11  if (D < 0) IP = $1
+12  if (D > 0) IP = $1
+13  if (D <= 0) IP = $1
+14  if (D >= 0) IP = $1
+15  if (D % 2 == 0) IP = $1
+16  if (D % 2 != 0) IP = $1
+
 2   *1 = *1 + 1
 20  *1 = *1 + $2
 21  *1 = *1 + *2
+
+3   *1 = *1 - 1
+30  *1 = *1 - $2
+31  *1 = *1 - *2
+
+4   *1 = *2
+
+40  *1 = *1 * $2
+41  *1 = *1 * *2
+
+50  *1 = Math.floor(*1 / $2)
+51  *1 = Math.floor(*1 / *2)
+
+60  *1 = *1 % $2
+61  *1 = *1 % *2
+
+7   D = *1
+70  D = *1 - $2
+71  D = *1 - *2
+
+8   alert(*1)
+9   *1 = parseInt(prompt()||0)
 `
-
-const proc = {
-  opBad() {
-    throw new Error('Bad opcode!')
-  },
-
-  op0() {},
-
-  op1(arg) {
-    this.nextOpAddr = arg
-  },
-  op10(arg) {
-    if (this.diff === 0)
-      this.nextOpAddr = arg
-  },
-  op100(arg) {
-    if (this.diff !== 0)
-      this.nextOpAddr = arg
-  },
-  op11(arg) {
-    if (this.diff < 0)
-      this.nextOpAddr = arg
-  },
-  op12(arg) {
-    if (this.diff > 0)
-      this.nextOpAddr = arg
-  },
-  op13(arg) {
-    if (this.diff <= 0)
-      this.nextOpAddr = arg
-  },
-  op14(arg) {
-    if (this.diff >= 0)
-      this.nextOpAddr = arg
-  },
-  op15(arg) {
-    if (this.diff % 2 === 0)
-      this.nextOpAddr = arg
-  },
-  op16(arg) {
-    if (this.diff % 2 !== 0)
-      this.nextOpAddr = arg
-  },
-
-  op2(arg) {
-    this.set(arg, this.get(arg) + 1)
-  },
-  op20(arg1, arg2) {
-    this.set(arg1, this.get(arg1) + arg2)
-  },
-  op21(arg1, arg2) {
-    this.set(arg1, this.get(arg1) + this.get(arg2))
-  },
-
-  op3(arg) {
-    this.set(arg, this.get(arg) - 1)
-  },
-  op30(arg1, arg2) {
-    this.set(arg1, this.get(arg1) - arg2)
-  },
-  op31(arg1, arg2) {
-    this.set(arg1, this.get(arg1) - this.get(arg2))
-  },
-
-  op4(arg1, arg2) {
-    this.set(arg1, this.get(arg2))
-  },
-
-  op40(arg1, arg2) {
-    this.set(arg1, this.get(arg1) * arg2)
-  },
-  op41(arg1, arg2) {
-    this.set(arg1, this.get(arg1) * this.get(arg2))
-  },
-
-  op50(arg1, arg2) {
-    this.set(arg1, Math.floor(this.get(arg1) / arg2))
-  },
-  op51(arg1, arg2) {
-    this.set(arg1, Math.floor(this.get(arg1) / this.get(arg2)))
-  },
-
-  op60(arg1, arg2) {
-    this.set(arg1, this.get(arg1) % arg2)
-  },
-  op61(arg1, arg2) {
-    this.set(arg1, this.get(arg1) % this.get(arg2))
-  },
-
-  op7(arg1) {
-    this.diff = this.get(arg1)
-  },
-  op70(arg1, arg2) {
-    this.diff = this.get(arg1) - arg2
-  },
-  op71(arg1, arg2) {
-    this.diff = this.get(arg1) - this.get(arg2)
-  },
-
-  op8(arg) {
-    alert(this.get(arg))
-  },
-  op9(arg) {
-    let num
-    do {
-      num = parseInt(prompt())
-    } while(isNaN(num))
-    this.set(arg, num)
-  }
-}
 
 const initMemory = [2,6, 2,6, 1,6, 0, 2,0, 1,0]
 
@@ -180,6 +101,9 @@ module.exports = {
   },
 
   methods: {
+    opBad() {
+      throw new Error('Bad opcode!')
+    },
     getN(addr, n) {
       return this.memory.slice(addr, addr + n)
     },
@@ -190,7 +114,7 @@ module.exports = {
       Vue.set(this.memory, addr, value)
     },
     getOp(opCode){
-      return (proc['op' + opCode] || proc.opBad).bind(this)
+      return (this.rules[opCode] || this.opBad).bind(this)
     },
     opLen(opCode){
       return this.getOp(opCode).length + 1
@@ -217,23 +141,37 @@ module.exports = {
       this.getOp(opCode)(...args)
     },
 
-    newMemory() {
-      this.memory = this.rawMemory.trim().split(/\s+/).map(Number)
+    resetState() {
       this.nextOpAddr = 0
+      this.diff = 0
+    },
+
+    newMemory() {
+      this.resetState()
+      this.memory = this.rawMemory.trim().split(/\s+/).map(Number)
     },
 
     newRules() {
+      this.resetState()
       this.rawRules = this.rawRules.trim().replace(/\n+/, '\n')
+      const rules = {}
       for (let rawRule of this.rawRules.split(/\n/)) {
-        let [opcode, ...tokens] = rawRule.split(/\s+/)
-        tokens = tokens.join(' ')
-        this.rules[opcode] = tokens
+        let [opcode, ...tokens] = rawRule.trim().split(/\s+/)
+        tokens = tokens.join('')
+        const argsNum = Math.max(...(tokens.match(/[$*]\d+/g) || ['00']).map(s => Number(s.slice(1))))
+        tokens = tokens
+          .replace(/nop/, '')
+          .replace(/(.*)(IP|D)=(.*)/, '$1$2@EQ$3')
           .replace('IP', 'this.nextOpAddr')
           .replace('D', 'this.diff')
-          .replace(/(.*)=(.*)/, 'this.set($1, $2)')
+          .replace(/\*(\d+)=(.*)/, 'this.set($$$1, $2)')
+          .replace(/@EQ/, '=')
           .replace(/\*(\d+)/g, 'this.get(arg$1)')
           .replace(/\$(\d+)/g, 'arg$1')
+          .replace(/(.*)(if\([^)]*\))(.*)/g, '$2$1$3')
+        rules[opcode] = Function(...[...Array(argsNum).keys()].map(n => 'arg' + (n+1)).concat([tokens]))
       }
+      this.rules = rules
     },
   },
 }
